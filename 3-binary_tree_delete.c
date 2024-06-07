e1a4bf18e7ae43abbd98e1e3cdc0ec24070a833c
IA-LINK <jyc.kom@gmail.com>
2024-06-07 00:57:06 -0400

    add 7, 3 and 8

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,59 +1,16 @@
-
-
-
-
-
-
-
-
-
 #include "binary_trees.h"
 
-void swap(int *x, int *y);
-void quick_sort_recursion(int *array, int start, int end, size_t size);
-int partition(int *array, int start, int end, size_t size);
+/**
+ * binary_tree_delete - deletes a binary tree
+ * @tree: the tree to delete
+ * Return: void
+ */
 
-void quick_sort(int *array, size_t size)
+void binary_tree_delete(binary_tree_t *tree)
 {
-	quick_sort_recursion(array, 0, size - 1, size);
-}
-
-void quick_sort_recursion(int *array, int start, int end, size_t size)
-{
-	int pivot_index;
-
-	if (start >= end)
+	if (tree == NULL)
 		return;
-	pivot_index = partition(array, start, end, size);
-
-	quick_sort_recursion(array, start, pivot_index - 1, size);
-	quick_sort_recursion(array, pivot_index + 1, end, size);
-}
-
-int partition(int *array, int start, int end, size_t size)
-{
-	int pivot_value = array[end];
-	int index = start;
-	int j = 0;
-
-	for (j = start; j < end; j++)
-	{
-		if (array[j] <= pivot_value)
-		{
-			swap(&array[j], &array[index]);
-			index++;
-		}
-	}
-	swap(&array[index], &array[end]);
-	print_array(array, size);
-	return(index);
-}
-
-void swap(int *x, int *y)
-{
-	int temp;
-	
-	temp = *x;
-	*x = *y;
-	*y = temp;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_postoder - goes through binary tree using post-order
- * @tree: A pointer to the root node
- * @func: A pointer to the func to call node
-*/
+ * binary_tree_postorder - postorder traversal
+ * @tree: pointer to the root ofthe tree
+ * @func: pointer to the function to call
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
