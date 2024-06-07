5ea7ee5f1ae55bc8085ae6c592b22c13e7ccd081
IA-LINK <jyc.kom@gmail.com>
2024-06-07 00:47:48 -0400

    Add 3 and 6 file

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_preorder - traverses through the tree using pre-order
- * @tree: Pointer to the root node of the tree
- * @func: pointer to a function to call
-*/
+ * binary_tree_preorder - preorder traversal
+ * @tree: pinterr to the root nodee
+ * @func: pointer toa a function to call
+ * Return: void
+ */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return; /* do nothing if the tree or func is null*/
-    }
-    func(tree->n);
+	if (tree == NULL || func == NULL)
+		return;
 
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
-
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
